b60f4ec91ab7f56d42194608a09d9c7f00a581b7
soaliyek <soaliye.kindo@ashesi.edu.gh>
2025-12-11 01:02:10 +0000

    Creating Structure

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+    // Create Database Connection Infomation
+    $env = parse_ini_file(__DIR__ . "/../env/connect.env");
+
+    // Create a connection
+    $connection = new mysqli($env["server"], $env["user"], $env["password"], $env["database"], $env["port"]);
+
+    // Check if connection established
+    if($connection->connect_error){
+        die("Connection Failed " . $connection->connect_error);
+    }
+
+    // Message
+    echo "Connected Successfully to the Database!\n";
+?>
